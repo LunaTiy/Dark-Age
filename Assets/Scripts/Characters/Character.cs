816bee81b79407a046b1a4a612d8e78f8e85f75c
LunaTiy <savushkin-daniil-99@yandex.ru>
2022-05-04 20:30:31 +0300

    Add characteristics class instead Character fields

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -4,78 +4,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Character : MonoBehaviour, ICharacter
+public class Character : MonoBehaviour
 {
+	[Header("Events")]
+	[SerializeField] private UnityEvent<float> _takedDamage;
 	public event Action<int, int> HealthChanged;
 	public event Action<int, int> ManaChanged;
 
-	[SerializeField] private int _maxHealth = 100;
-	[SerializeField] private int _health = 100;
-	[SerializeField] private int _maxMana = 20;
-	[SerializeField] private int _mana = 20;
-
-	private Stats _passiveEffects;
-
-	public int MaxHealth
-	{
-		get => _maxHealth;
-		set
-		{
-			if (value > 0) _maxHealth = value;
-		}
-	}
-	public int Health
-	{
-		get => _health;
-		set
-		{
-			if (value >= 0 && value <= _maxHealth) _health = value;
-			else if (value < 0) _health = 0;
-		}
-	}
-	public int MaxMana
-	{
-		get => _maxMana;
-		set
-		{
-			if (value > 0) _maxMana = value;
-		}
-	}
-	public int Mana
-	{
-		get => _mana;
-		set
-		{
-			if (value >= 0 && value <= _maxMana) _mana = value;
-			else if (value < 0) _mana = 0;
-		}
-	}
+	private Characteristics _characteristics;	
+	private Stats _passives;
 
 	private void Start()
 	{
-		_passiveEffects = new Stats(new List<Stat> { 
-			new Stat(1, Stat.TypeStat.HealthRegeneration),
-			new Stat(1, Stat.TypeStat.ManaRegeneration)
+		_characteristics = new Characteristics();
+
+		_passives = new Stats(new List<Stat> { 
+			new StatHealthRegeneration(1, -1),
+			new StatManaRegeneration(1, -1)
 		});
 
-		StartCoroutine(PassiveSkills());
+		StartCoroutine(InfluencePassives());
 	}
 
-	private IEnumerator PassiveSkills()
+	private IEnumerator InfluencePassives()
 	{
 		var waitForSeconds = new WaitForSeconds(2f);
 
 		while (true)
 		{
-			foreach (Stat stat in _passiveEffects.GetStats())
-			{
-				if (stat.type == Stat.TypeStat.HealthRegeneration) Health += stat.value;
-				else if (stat.type == Stat.TypeStat.ManaRegeneration) Mana += stat.value;
-				else Health -= stat.value;
-			}
+			_passives.Influence(_characteristics);
 
-			HealthChanged?.Invoke(Health, MaxHealth);
-			ManaChanged?.Invoke(Mana, MaxMana);
+			HealthChanged?.Invoke(_characteristics.Health, _characteristics.MaxHealth);
+			ManaChanged?.Invoke(_characteristics.Mana, _characteristics.MaxMana);
 
 			yield return waitForSeconds;
 		}
@@ -83,7 +43,17 @@ public class Character : MonoBehaviour, ICharacter
 
 	public void TakeDamage(int damage)
 	{
-		Health -= damage;
-		HealthChanged?.Invoke(_health, _maxHealth);
+		_characteristics.Health -= damage;
+		HealthChanged?.Invoke(_characteristics.Health, _characteristics.MaxHealth);
+
+		_takedDamage?.Invoke(0.2f);
+	}
+
+	public void GetPassiveEffect(Stat stat)
+	{
+		if (stat == null)
+			return;
+
+		_passives.AddStat(stat);
 	}
 }
---
+++ Assets/Scripts/Characters/Characteristics.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Characteristics
+{
+	private int _maxHealth;
+	private int _health;
+	private int _maxMana;
+	private int _mana;
+
+	public Characteristics()
+	{
+		_maxHealth = 100;
+		_health = 100;
+		_maxMana = 20;
+		_mana = 20;
+	}
+
+	public Characteristics(int maxHealth, int health, int maxMana, int mana)
+	{
+		MaxHealth = maxHealth;
+		Health = health;
+		MaxMana = maxMana;
+		Mana = mana;
+	}
+
+	public int MaxHealth
+	{
+		get => _maxHealth;
+		set
+		{
+			if (value > 0) _maxHealth = value;
+		}
+	}
+	public int Health
+	{
+		get => _health;
+		set
+		{
+			if (value >= 0 && value <= _maxHealth) _health = value;
+			else if (value < 0) _health = 0;
+		}
+	}
+	public int MaxMana
+	{
+		get => _maxMana;
+		set
+		{
+			if (value > 0) _maxMana = value;
+		}
+	}
+	public int Mana
+	{
+		get => _mana;
+		set
+		{
+			if (value >= 0 && value <= _maxMana) _mana = value;
+			else if (value < 0) _mana = 0;
+		}
+	}
+}
