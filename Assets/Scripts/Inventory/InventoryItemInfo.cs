885ba0801650e9ea833f8ddd4d42bcd08b691556
LunaTiy <savushkin-daniil-99@yandex.ru>
2022-05-06 23:02:44 +0300

    Add inplementation IInvetoryItemInfo and IInventoryItemState

---
+++ Assets/Scripts/Inventory/InventoryItemInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "InventoryItemInfo", menuName = "Gameplay/Items/Create ItemInfo")]
+class InventoryItemInfo : ScriptableObject, IInventoryItemInfo
+{
+	[SerializeField] private string _id;
+	[SerializeField] private string _title;
+	[SerializeField] private string _description;
+	[SerializeField] private int _maxItemInSlots;
+	[SerializeField] private Sprite _spriteIcon;
+
+	public string Id => _id;
+	public string Title => _title;
+	public string Description => _description;
+	public int MaxItemInSlots => _maxItemInSlots;
+	public Sprite SpriteIcon => _spriteIcon;
+}
---
+++ Assets/Scripts/Inventory/InventoryItemState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+[Serializable]
+class InventoryItemState : IInventoryItemState
+{
+	private int _amount;
+	private bool _isEquipped;
+
+	public InventoryItemState()
+	{
+		_amount = 0;
+		_isEquipped = false;
+	}
+
+	public int Amount { get => _amount; set => _amount = value; }
+	public bool IsEquipped { get => _isEquipped; set => _isEquipped = value; }
+}
