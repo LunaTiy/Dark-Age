3e6527424f3b6893b0b1de85e3d67809aa3f57cd
LunaTiy <savushkin-daniil-99@yandex.ru>
2022-05-27 22:41:34 +0300

    Add spawner for trees

---
+++ Assets/Scripts/Collactable Items/Spawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private float _minTime;
+    [SerializeField] private float _maxTime;
+	[SerializeField] private int _maxSpawnerItems;
+
+	private void Start()
+	{
+		StartCoroutine(Spawn());
+	}
+
+	private IEnumerator Spawn()
+	{
+		while (true)
+		{
+			if(transform.childCount <= _maxSpawnerItems)
+				Instantiate(_prefab, transform);
+
+			yield return new WaitForSeconds(Random.Range(_minTime, _maxTime));
+		}
+	}
+}
